ea3a3621f253e99e0e20f7cec6933afe3a487abc
nickforbizz <nickforbizz@gmail.com>
2021-06-29 00:36:59 +0300

    composer install storage on server

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="{{ route('welcome') }}">
+            <a href="{{ route('welcome') }}" >
                 Go Back Home
             </a>
         </x-slot>
