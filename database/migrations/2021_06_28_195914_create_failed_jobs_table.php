961268c38f71b7521287043b84f1bcbd03682540
Nick <nickforbizz@gmail.com>
2021-09-28 23:03:12 +0300

    uuid

--- database/migrations/2021_06_28_195914_create_failed_jobs_table.php
+++ database/migrations/2021_06_28_195914_create_failed_jobs_table.php
@@ -15,7 +15,7 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->integer('id', true);
-            $table->string('uuid')->unique();
+            $table->string('uuid')->unique(6);
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
